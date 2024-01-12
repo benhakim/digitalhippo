114c7f69c1ea1916d685704d661d499a74ff734e
benhakim <lhkim@web.de>
2024-01-12 14:55:33 +0100

    change1

---
+++ src/config/index.ts
@@ -0,0 +1,45 @@
+
+ export const PRODUCT_CATEGORIES = [
+     {
+    label: 'UI Kits',
+    value: 'UI_Kits' as const,
+    featured: [
+        {
+            name: 'Editor picks',
+            href: '#',
+            imageSrc: '/nav/ui-kits/mixed.jpg',
+        },
+        {
+            name: 'New Arrival',
+            href: '#',
+            imageSrc: '/nav/ui-kits/blue.jpg',
+        },
+        {
+            name: 'Bestsellers',
+            href: '#',
+            imageSrc: '/nav/ui-kits/purple.jpg',
+        },
+    ]
+     },
+     {
+     label: 'Icons',
+     value: 'Icons' as const,
+     featured: [
+     {
+         name: 'Favorite icon picks',
+         href: '#',
+         imageSrc: '/nav/icons/picks.jpg',
+     },
+     {
+         name: 'New Arrival',
+         href: '#',
+         imageSrc: '/nav/icons/new.jpg',
+     },
+     {
+         name: 'Bestselling Icons',
+         href: '#',
+         imageSrc: '/nav/icons/bestsellers.jpg',
+     },
+ ]
+ }
+ ]
